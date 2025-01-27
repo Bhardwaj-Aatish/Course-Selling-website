389ebb86b717d123084da9e41c29301cc0bd620c
unknown <bhardwaj-aatish>
2025-01-27 21:56:55 +0530

    Added skeleton for index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("Getting our express app");
-});
+app.post('/user/signup', (req, res) => {
+
+})
+
+app.post('/user/signin', (req, res) => {
+  
+})
+
+app.get('user/purchases', (req, res) => {
+
+})
+
+app.post('/course/purchase', (req, res) => {
+
+})
+
+app.get('/course/preview', (req, res) => {
+
+})
 
 app.listen(3000);
